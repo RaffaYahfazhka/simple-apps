27e151b1401e70b475788f7c49fafc9b360e8502
raffa <muhammadraffayahfazhka@gmail.com>
2024-03-03 11:53:09 +0700

    update app.test.js

--- testing/app.test.js
+++ testing/app.test.js
@@ -26,6 +26,13 @@ describe('Unit Test /app1', () => {
     });
   });
 
+  describe('Unit test /about', () => {
+    it('should respond with "Hello my name is raffa"', async () => {
+      const response = await request(app).get('/about');
+      expect(response.status).toBe(200);
+    });
+  });
+
 describe('Integration Test Connect Database', () => {
   beforeEach(() => {
     // Set the connection for the application to use
